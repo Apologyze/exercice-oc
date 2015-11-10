8d4680e11d2fc58fd8344ca23461ce83b83b2bc6
Apologyze <kev.mez@gmail.com>
2015-11-10 10:45:00 +0100

    Ajout d'une page d'accueil de présentation

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Page de démonstration Git</title>
+</head>
+<body>
+	<h1>Ceci est une page de démonstration</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum nisi ad ab nemo ratione, id soluta, aspernatur nulla minus sit?</p>
+	<ul>
+		<li>Lorem ipsum dolor sit amet.</li>
+		<li>Lorem ipsum dolor sit amet.</li>
+		<li>Lorem ipsum dolor sit amet.</li>
+		<li>Lorem ipsum dolor sit amet.</li>
+		<li>Lorem ipsum dolor sit amet.</li>
+		<li>Lorem ipsum dolor sit amet.</li>
+	</ul>
+</body>
+</html>
